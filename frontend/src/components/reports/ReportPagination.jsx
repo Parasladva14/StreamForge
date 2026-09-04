0ce203f90b1c264e93fff072238be1e1de36d867
Paras Ladva <parasladva14@gmail.com>
2026-09-04 18:38:15 +0530

    Add ReportPagination.jsx in frontend\src\components\reports

---
+++ frontend/src/components/reports/ReportPagination.jsx
@@ -0,0 +1,47 @@
+import "./ReportPagination.css";
+
+export default function ReportPagination({
+
+    currentPage,
+
+    totalPages,
+
+    onPageChange,
+
+}) {
+
+    if (totalPages <= 1) return null;
+
+    return (
+
+        <div className="pagination">
+
+            <button
+                disabled={currentPage === 1}
+                onClick={() =>
+                    onPageChange(currentPage - 1)
+                }
+            >
+                ◀ Previous
+            </button>
+
+            <span>
+
+                Page {currentPage} of {totalPages}
+
+            </span>
+
+            <button
+                disabled={currentPage === totalPages}
+                onClick={() =>
+                    onPageChange(currentPage + 1)
+                }
+            >
+                Next ▶
+            </button>
+
+        </div>
+
+    );
+
+}
