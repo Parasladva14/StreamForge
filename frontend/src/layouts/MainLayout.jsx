931fd21a96dba9bf7a0c50e9636cd1bade4ba1ab
Paras Ladva <parasladva14@gmail.com>
2026-09-04 18:38:23 +0530

    Add MainLayout.jsx in frontend\src\layouts

---
+++ frontend/src/layouts/MainLayout.jsx
@@ -0,0 +1,31 @@
+import { Outlet } from "react-router-dom";
+
+import Navbar from "../components/layout/Navbar";
+import Sidebar from "../components/layout/Sidebar";
+
+function MainLayout() {
+    return (
+        <>
+            <Navbar />
+
+            <div
+                style={{
+                    display: "flex",
+                }}
+            >
+                <Sidebar />
+
+                <main
+                    style={{
+                        flex: 1,
+                        padding: "20px",
+                    }}
+                >
+                    <Outlet />
+                </main>
+            </div>
+        </>
+    );
+}
+
+export default MainLayout;
