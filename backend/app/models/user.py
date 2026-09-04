072fcf5a219c41ef89736496ce861e4e39ab6b98
Paras Ladva <parasladva14@gmail.com>
2026-09-04 18:37:28 +0530

    Add user.py in backend\app\models

---
+++ backend/app/models/user.py
@@ -0,0 +1,23 @@
+from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy.sql import func
+
+from app.database.base import Base
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+
+    username = Column(String(100), nullable=False)
+
+    email = Column(String(255), unique=True, nullable=False)
+
+    password = Column(String(255), nullable=False)
+
+    role = Column(String(50), default="Admin")
+
+    created_at = Column(
+        DateTime(timezone=True),
+        server_default=func.now(),
+    )
